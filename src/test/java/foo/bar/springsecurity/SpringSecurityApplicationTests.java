f787411263b396d1cf3e6a19e66ec047eb654001
Alexander Kramer <a.kramer@reply.de>
2023-02-06 12:41:17 +0100

    :tada: initial commit

---
+++ src/test/java/foo/bar/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package foo.bar.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
