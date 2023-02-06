f1830e0c9039dc1e6023e2f9336491f05940971e
Alexander Kramer <a.kramer@reply.de>
2023-02-06 12:45:20 +0100

    :sparkles: add demo endpoint

---
+++ src/main/java/foo/bar/springsecurity/DemoController.java
@@ -0,0 +1,13 @@
+package foo.bar.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
